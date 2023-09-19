63e19e1090351d9ada205f5522af381194b2a354
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-09-19 11:36:53 +0400

    Add files via upload

---
+++ Save.php
@@ -0,0 +1,31 @@
+<?php
+// Receive data from the Python script via HTTP POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Data sent
+    $data = $_POST;
+
+    // Generate a unique filename based on the current timestamp
+    $timestamp = time();
+    $file_path = "data_$timestamp.txt";
+
+    // Open the file for writing (create if it doesn't exist)
+    $file = fopen($file_path, "w");
+
+    if ($file) {
+        // Convert data to a text string and store it in the file
+        fwrite($file, json_encode($data, JSON_PRETTY_PRINT));
+
+        // Close the file
+        fclose($file);
+
+        // Send a success response with the filename
+        echo "Data received and saved successfully in $file_path";
+    } else {
+        // If unable to open the file for writing
+        echo "Failed to open the file for writing.";
+    }
+} else {
+    // If no POST requests found
+    echo "No POST Requests Found.";
+}
+?>
