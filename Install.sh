0da1a81d24ecabe5dd21ce6eb6da7ba9dbf94204
AbdulazizDXT <143551117+AzizDXT@users.noreply.github.com>
2023-09-20 16:51:48 +0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,23 +1,20 @@
 sudo apt-get update -y
-sudo apt-get install python3 python2 python3-pip figlet lolcat git php dbus-x11 gnome-terminal -y ; sudo apt-get update -y ; pip install re cryptography requests base64 json time os threading ; sudo apt-get install python3 figlet ; sudo apt-get install python3 python2 python3-pip figlet lolcat git php dbus-x11 gnome-terminal -y
+sudo apt-get install python3 python2 python3-pip figlet lolcat git php dbus-x11 gnome-terminal tar -y
+pip install  cryptography requests json time os threading folium geopy psutil wmi Dispatch ping3 termcolor re base64
+sudo apt-get update -y 
 mkdir -p ~/.local/share/fonts/figlet-fonts/
 git clone https://github.com/xero/figlet-fonts.git ~/.local/share/fonts/figlet-fonts/
 clear
 figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Installing... | lolcat
-pip install folium geopy psutil wmi Dispatch ping3 termcolor
 wget https://github.com/ekzhang/bore/releases/download/v0.5.0/bore-v0.5.0-x86_64-unknown-linux-musl.tar.gz
+tar xzvf bore-v0.5.0-x86_64-unknown-linux-musl.tar.gz
 clear
 sleep 1
 figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Installing... | lolcat
-sudo apt install tar
-tar xzvf bore-v0.5.0-x86_64-unknown-linux-musl.tar.gz
 rm -rf figlet-fonts bore-v0.5.0-x86_64-unknown-linux-musl.tar.gz
-wget -P ~/.local/share/fonts/figlet-fonts/ https://raw.githubusercontent.com/xero/figlet-fonts/master/ANSI%20Regular.flf 
-mv ~/.local/share/fonts/figlet-fonts/'ANSI Regular.flf' Reg.flf
+wget -O ~/.local/share/fonts/figlet-fonts/Reg.flf https://raw.githubusercontent.com/xero/figlet-fonts/master/ANSI%20Regular.flf
 clear
-figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Installed! | lolcat
 sleep 2
-clear
 figlet -c -f ~/.local/share/fonts/figlet-fonts/3d.flf Finish! | lolcat
 sleep 2
 clear
@@ -31,4 +28,3 @@ echo Running Service...
 sleep 1
 clear
 gnome-terminal -- python3 Chat.py
-
